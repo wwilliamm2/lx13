36a19638dd097e3b52445dabaac695d478806f4e
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-05-30 11:24:23 -0700

    more

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def about
+  end
+
+  def blog
+  end
+
+  def contact
+  end
+
+  def ldoc
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "home/about"
+  get "home/blog"
+  get "home/contact"
+  get "home/ldoc"
+
+  get  '/about',   to: 'home#about'
+  get  '/blog',    to: 'home#blog'
+  get  '/contact', to: 'home#contact'
+  get  '/examp',   to: 'home#examp'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -10,5 +20,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#about"
 end
